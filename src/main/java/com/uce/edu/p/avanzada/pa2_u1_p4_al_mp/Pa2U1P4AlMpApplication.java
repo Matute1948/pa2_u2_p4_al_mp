5addfa1e573d8f959bd35a5047a09d0228a1858f
Matute1948  <achigfelipe@gmail.com>
2023-06-20 12:13:43 -0500

    Tarea04

--- src/main/java/com/uce/edu/p/avanzada/pa2_u1_p4_al_mp/Pa2U1P4AlMpApplication.java
+++ src/main/java/com/uce/edu/p/avanzada/pa2_u1_p4_al_mp/Pa2U1P4AlMpApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.p.avanzada.pa2_u1_p4_al_mp;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,8 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.repository.modelo.Autor;
+import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.repository.modelo.Ciudadano;
+import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.repository.modelo.Empleado;
 import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.repository.modelo.Libro;
 import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.service.AutorService;
+import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.service.CiudadanoService;
+import com.uce.edu.p.avanzada.pa2_u1_p4_al_mp.service.EmpleadoService;
 
 
 
@@ -19,7 +24,10 @@ public class Pa2U1P4AlMpApplication implements CommandLineRunner {
 
 	@Autowired
 	private AutorService autorService;
-	
+	@Autowired
+	private CiudadanoService ciudadanoService;
+	@Autowired
+	private EmpleadoService empleadoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P4AlMpApplication.class, args);
@@ -27,7 +35,33 @@ public class Pa2U1P4AlMpApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		Ciudadano ciudadano = new Ciudadano();
+		Empleado empleado = new Empleado();
+
+		ciudadano.setNombre("Andres");
+		ciudadano.setApellido("Lugmaña");
+		ciudadano.setCedula("17242106985");
+		ciudadano.setEmpleado(empleado);
+
+		empleado.setCargo("Programador");
+		empleado.setCiudadano(ciudadano);
+		empleado.setSueldo(new BigDecimal(2000));
+
+		this.ciudadanoService.agregar(ciudadano);
+		Ciudadano ciu = this.ciudadanoService.buscarPorNumero(1);
+		ciu.setNombre("Felipe");
+		this.ciudadanoService.actualizar(ciu);
 		
+		Empleado em = this.empleadoService.buscarPorNumero(1);
+		em.setCargo("Senior");
+		this.empleadoService.actualizar(em);
+		//this.empleadoService.eliminar(1);
+		//this.ciudadanoService.eliminar(1);
+
+
+
+		/* 
 		Autor autor = new Autor();
 		Autor autor1 = new Autor();
 
@@ -62,7 +96,7 @@ public class Pa2U1P4AlMpApplication implements CommandLineRunner {
 
 		this.autorService.agregar(autor);
 		
-		
+		*/
 
 	}
 
